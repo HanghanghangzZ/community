b6edcb36ac3f56a9fc917c8bb4e833f5200b5c3a
Hanghang <1005130450@qq.com>
2021-04-24 14:43:13 +0800

    init repo

---
+++ src/main/java/com/hang/myselfcommunity/MyselfCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.hang.myselfcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselfCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hang/myselfcommunity/MyselfCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hang.myselfcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselfCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
