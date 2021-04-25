13ae441f10913b1adaf5cc96181c9d281ed7ecb6
HanghanghangzZ <1005130450@qq.com>
2021-04-25 14:53:32 +0800

    添加了MySQL数据库，将登录信息存入session以及index的一些改动

--- src/main/java/com/hang/myselfcommunity/controller/AuthorizeController.java
+++ src/main/java/com/hang/myselfcommunity/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -37,7 +39,8 @@ public class AuthorizeController {
      */
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -47,7 +50,14 @@ public class AuthorizeController {
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
         System.out.println(user);
-        return "index";
+        if (user != null) {
+            /* 登录成功，保存session和cookie */
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            /* 登录失败，重新登录 */
+            return "redirect:/";
+        }
     }
 
 
